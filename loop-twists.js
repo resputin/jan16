b4e17c2d9675406e5fef103034ae8171c2574acc
Ahad Darkhosh <darkhosh@hotmail.com>
2018-01-16 09:40:52 -0800

    Min and Max were commented out and average function refactored using forEach)

--- loop-twists.js
+++ loop-twists.js
@@ -18,20 +18,36 @@
 // let maxVal = max(maxArray);
 // console.log(maxVal);
 
-function min(arr){
-  let currentMin = arr[0];
-  let i = 0;
-  while(i < arr.length) {
-    if (arr[i] < currentMin) {
-      currentMin = arr[i];
-    }
-    i++;
-  }
-  return currentMin;
+// function min(arr){
+//   let currentMin = arr[0];
+//   let i = 0;
+//   while(i < arr.length) {
+//     if (arr[i] < currentMin) {
+//       currentMin = arr[i];
+//     }
+//     i++;
+//   }
+//   return currentMin;
+// }
+
+
+// let minArray = [15, 2, 9, 10, 5, 6];
+
+// let minVal = min(minArray);
+// console.log(minVal);
+//-------------------------------------------------------------
+
+function average(numbers) {
+  // initially we set total to the value of the
+  // first number.
+  let total = 0;
+  // then we add each of the remaining numbers
+  // to total.
+  numbers.forEach(element=>total+=element);
+  // then we return total divided by the length
+  // of our numbers array to get the average.
+  console.log(total / numbers.length);
+  return total / numbers.length;
 }
 
-
-let minArray = [15, 2, 9, 10, 5, 6];
-
-let minVal = min(minArray);
-console.log(minVal);
+average([1,2,3,4,5,6,7,8]);
