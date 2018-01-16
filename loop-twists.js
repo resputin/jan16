5f209be5bbc2419c647534d8f02952366bcb2bb5
Ahad Darkhosh <darkhosh@hotmail.com>
2018-01-16 09:26:57 -0800

    Min. function added and Max. commented out

--- loop-twists.js
+++ loop-twists.js
@@ -1,19 +1,37 @@
-'use strict';
+// 'use strict';
 
-function max(arr){
-  let currentMax = arr[0];
+// function max(arr){
+//   let currentMax = arr[0];
+//   let i = 0;
+//   while(i < arr.length) {
+//     if (arr[i] > currentMax) {
+//       currentMax = arr[i];
+//     }
+//     i++;
+//   }
+//   return currentMax;
+// }
+
+
+// let maxArray = [15, 2, 9, 10, 5, 6];
+
+// let maxVal = max(maxArray);
+// console.log(maxVal);
+
+function min(arr){
+  let currentMin = arr[0];
   let i = 0;
   while(i < arr.length) {
-    if (arr[i] > currentMax) {
-      currentMax = arr[i];
+    if (arr[i] < currentMin) {
+      currentMin = arr[i];
     }
     i++;
   }
-  return currentMax;
+  return currentMin;
 }
 
 
-let maxArray = [15, 2, 9, 10, 5, 6];
+let minArray = [15, 2, 9, 10, 5, 6];
 
-let maxVal = max(maxArray);
-console.log(maxVal);
+let minVal = min(minArray);
+console.log(minVal);
