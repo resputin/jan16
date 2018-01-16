dc99b29c7220667007bfc05810b1b249d37a41fa
Dylan <nielsen.dylan@gmail.com>
2018-01-16 11:26:17 -0800

    Add hazard warning function

--- higher-order-function-drills.js
+++ higher-order-function-drills.js
@@ -1,4 +1,4 @@
-// 'use strict';
+'use strict';
 
 // function repeat(fn, n){
 //   for (let i = 0; i < n; i++) {
@@ -20,29 +20,47 @@
 // Return only names that begin with 'R'
 
 //-------------------------------------------------------------------
-const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+// const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 
-const filteredNames = filter(myNames, function(name) {
-    // This is a "predicate function" - it's a function that only returns a boolean
-    return name[0] === 'R';
-});
+// const filteredNames = filter(myNames, function(name) {
+//     // This is a "predicate function" - it's a function that only returns a boolean
+//     return name[0] === 'R';
+// });
 
-console.log(filteredNames) // => ['Rich', 'Ray']
-// <---- DO NOT EDIT BETWEEN THESE LINES
+// console.log(filteredNames) // => ['Rich', 'Ray']
+// // <---- DO NOT EDIT BETWEEN THESE LINES
 
-// TASK: DEFINE YOUR FILTER FUNCTION BELOW:
-function filter(arr,fn) {
-  const newArray=[];
-  for(let i=0;i<arr.length;i++){
-    if(fn(arr[i])){
-      newArray.push(arr[i]);
-    }
-  }
-  return newArray;
+// // TASK: DEFINE YOUR FILTER FUNCTION BELOW:
+// function filter(arr,fn) {
+//   const newArray=[];
+//   for(let i=0;i<arr.length;i++){
+//     if(fn(arr[i])){
+//       newArray.push(arr[i]);
+//     }
+//   }
+//   return newArray;
 
+// }
+// function checkFirstLetter(word){
+//   return word[0]==='R';
+// }
+
+// console.log(filter(myNames,checkFirstLetter));
+
+
+function hazardWarningCreator(typeOfWarning){
+  let warningCounter = 0;
+
+  return function(location){
+    warningCounter++;
+    console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+    console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+  };
 }
-function checkFirstLetter(word){
-  return word[0]==='R';
-}
 
-console.log(filter(myNames,checkFirstLetter));
+const warning1 = hazardWarningCreator("Rocks on the road");
+const warning2 = hazardWarningCreator("Icy roads");
+const warning3 = hazardWarningCreator("Don't fall off the cliff");
+
+warning1("Here");
+warning1("Here");
