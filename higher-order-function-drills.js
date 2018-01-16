0ac1909a256520e78347bb9f4bfbec0b36491865
Dylan <nielsen.dylan@gmail.com>
2018-01-16 11:27:31 -0800

    I should save

--- higher-order-function-drills.js
+++ higher-order-function-drills.js
@@ -48,19 +48,19 @@
 // console.log(filter(myNames,checkFirstLetter));
 
 
-function hazardWarningCreator(typeOfWarning){
-  let warningCounter = 0;
+// function hazardWarningCreator(typeOfWarning){
+//   let warningCounter = 0;
 
-  return function(location){
-    warningCounter++;
-    console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
-    console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
-  };
-}
+//   return function(location){
+//     warningCounter++;
+//     console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+//     console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+//   };
+// }
 
-const warning1 = hazardWarningCreator("Rocks on the road");
-const warning2 = hazardWarningCreator("Icy roads");
-const warning3 = hazardWarningCreator("Don't fall off the cliff");
+// const warning1 = hazardWarningCreator("Rocks on the road");
+// const warning2 = hazardWarningCreator("Icy roads");
+// const warning3 = hazardWarningCreator("Don't fall off the cliff");
 
-warning1("Here");
-warning1("Here");
+// warning1("Here");
+// warning1("Here");
