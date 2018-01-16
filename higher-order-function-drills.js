5b49919949af1a840685afbc0e77e31dfb5c541d
Ahad Darkhosh <darkhosh@hotmail.com>
2018-01-16 11:15:22 -0800

    Filter Function Created

--- higher-order-function-drills.js
+++ higher-order-function-drills.js
@@ -1,18 +1,48 @@
-'use strict';
+// 'use strict';
 
-function repeat(fn, n){
-  for (let i = 0; i < n; i++) {
-    fn();
+// function repeat(fn, n){
+//   for (let i = 0; i < n; i++) {
+//     fn();
+//   }
+// }
+
+// function hello() {
+//   console.log("Hello World");
+// }
+
+// function goodbye() {
+//   console.log("Goodbye World");
+// }
+
+// repeat(hello, 5);
+// repeat(goodbye, 5);
+// DO NOT EDIT BETWEEN THESE LINES ----->
+// Return only names that begin with 'R'
+
+//-------------------------------------------------------------------
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+const filteredNames = filter(myNames, function(name) {
+    // This is a "predicate function" - it's a function that only returns a boolean
+    return name[0] === 'R';
+});
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
+
+// TASK: DEFINE YOUR FILTER FUNCTION BELOW:
+function filter(arr,fn) {
+  const newArray=[];
+  for(let i=0;i<arr.length;i++){
+    if(fn(arr[i])){
+      newArray.push(arr[i]);
+    }
   }
+  return newArray;
+
+}
+function checkFirstLetter(word){
+  return word[0]==='R';
 }
 
-function hello() {
-  console.log("Hello World");
-}
-
-function goodbye() {
-  console.log("Goodbye World");
-}
-
-repeat(hello, 5);
-repeat(goodbye, 5);
+console.log(filter(myNames,checkFirstLetter));
