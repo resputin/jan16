c4146b135f1c4bc9ae2c8ae7c4b74e5a9fc9d009
Dylan <nielsen.dylan@gmail.com>
2018-01-16 10:37:35 -0800

    Uncomment

--- loop-twists.js
+++ loop-twists.js
@@ -1,41 +1,41 @@
 'use strict';
 
-// function max(arr){
-//   let currentMax = arr[0];
-//   let i = 0;
-//   while(i < arr.length) {
-//     if (arr[i] > currentMax) {
-//       currentMax = arr[i];
-//     }
-//     i++;
-//   }
-//   return currentMax;
-// }
+function max(arr){
+  let currentMax = arr[0];
+  let i = 0;
+  while(i < arr.length) {
+    if (arr[i] > currentMax) {
+      currentMax = arr[i];
+    }
+    i++;
+  }
+  return currentMax;
+}
 
 
-// let maxArray = [15, 2, 9, 10, 5, 6];
+let maxArray = [15, 2, 9, 10, 5, 6];
 
-// let maxVal = max(maxArray);
-// console.log(maxVal);
+let maxVal = max(maxArray);
+console.log(maxVal);
 
-// function min(arr){
-//   let currentMin = arr[0];
-//   let i = 0;
-//   while(i < arr.length) {
-//     if (arr[i] < currentMin) {
-//       currentMin = arr[i];
-//     }
-//     i++;
-//   }
-//   return currentMin;
-// }
+function min(arr){
+  let currentMin = arr[0];
+  let i = 0;
+  while(i < arr.length) {
+    if (arr[i] < currentMin) {
+      currentMin = arr[i];
+    }
+    i++;
+  }
+  return currentMin;
+}
 
 
-// let minArray = [15, 2, 9, 10, 5, 6];
+let minArray = [15, 2, 9, 10, 5, 6];
 
-// let minVal = min(minArray);
-// console.log(minVal);
-//-------------------------------------------------------------
+let minVal = min(minArray);
+console.log(minVal);
+// -------------------------------------------------------------
 
 function average(numbers) {
   let total = 0;
