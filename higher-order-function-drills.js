617daa8330c99b802beaecdb23d4705b31fce60c
Dylan <nielsen.dylan@gmail.com>
2018-01-16 12:35:09 -0800

    Complete Reduce Function

--- higher-order-function-drills.js
+++ higher-order-function-drills.js
@@ -67,12 +67,48 @@
 
 
 
-const turtleArray=[[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
-function turtlemoves(arr){
-  const positiveMoves=arr.filter(move=>move[0]>=0 && move[1]>=0);
-  // console.log(positiveMoves);
-  const turtleSteps=positiveMoves.map(move=>move[0]+move[1]);
-  // console.log(turtleSteps);
-  turtleSteps.forEach(move=>console.log(move));
+// const turtleArray=[[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
+// function turtlemoves(arr){
+//   const positiveMoves=arr.filter(move=>move[0]>=0 && move[1]>=0);
+//   // console.log(positiveMoves);
+//   const turtleSteps=positiveMoves.map(move=>move[0]+move[1]);
+//   // console.log(turtleSteps);
+//   turtleSteps.forEach(move=>console.log(move));
+// }
+// turtlemoves(turtleArray);
+
+let secretWords =
+  "noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest";
+
+function secretMessage(str){
+  let wordArray = str.split(" ");
+  // console.log(wordArray);
+  return wordArray.reduce(function(total, currentValue){
+    // console.log(currentValue);
+    if (currentValue.length === 3) {
+      return total + " ";
+    } else {
+      // console.log(total);
+      // console.log(total + currentValue[currentValue.length - 1].toUpperCase());
+      return total + currentValue[currentValue.length - 1].toUpperCase();
+      // 
+    }
+  }, "");
 }
-turtlemoves(turtleArray);
+console.log(secretMessage(secretWords));
+
+
+//  let wordArray = secretWords.split(" ");
+
+//  console.log(wordArray.reduce((total, currentValue) => {
+//      if (currentValue.length === 3) {
+//        return total + " ";
+//      } else {
+//        return total + currentValue[currentValue.length - 1].toUpperCase();
+//      }
+//    }, ""));
+
+
+// function() {
+//   wordArray.reduce("NO NEED TO SHOUT");
+// }
