31ea000999111098c0d0ac2b9bb265b1d8f33f68
Dylan <nielsen.dylan@gmail.com>
2018-01-17 07:28:19 -0800

    Add a single line reduce refactor

--- higher-order-function-drills.js
+++ higher-order-function-drills.js
@@ -1,54 +1,54 @@
 'use strict';
 
-function repeat(fn, n){
-  for (let i = 0; i < n; i++) {
-    fn();
-  }
-}
+// function repeat(fn, n){
+//   for (let i = 0; i < n; i++) {
+//     fn();
+//   }
+// }
 
-function hello() {
-  console.log("Hello World");
-}
+// function hello() {
+//   console.log("Hello World");
+// }
 
-function goodbye() {
-  console.log("Goodbye World");
-}
+// function goodbye() {
+//   console.log("Goodbye World");
+// }
 
-repeat(hello, 5);
-repeat(goodbye, 5);
+// repeat(hello, 5);
+// repeat(goodbye, 5);
 
 
-// DO NOT EDIT BETWEEN THESE LINES ----->
-// Return only names that begin with 'R'
+// // DO NOT EDIT BETWEEN THESE LINES ----->
+// // Return only names that begin with 'R'
 
-//-------------------------------------------------------------------
-// const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+// //-------------------------------------------------------------------
+// // const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 
-// const filteredNames = filter(myNames, function(name) {
-//     // This is a "predicate function" - it's a function that only returns a boolean
-//     return name[0] === 'R';
-// });
+// // const filteredNames = filter(myNames, function(name) {
+// //     // This is a "predicate function" - it's a function that only returns a boolean
+// //     return name[0] === 'R';
+// // });
 
-// //console.log(filteredNames) // => ['Rich', 'Ray']
-// // <---- DO NOT EDIT BETWEEN THESE LINES
+// // //console.log(filteredNames) // => ['Rich', 'Ray']
+// // // <---- DO NOT EDIT BETWEEN THESE LINES
 
-// TASK: DEFINE YOUR FILTER FUNCTION BELOW:
-function filter(arr,fn) {
-  const newArray=[];
-  for(let i=0;i<arr.length;i++){
-    if(fn(arr[i])){
-      newArray.push(arr[i]);
-    }
-  }
-  return newArray;
+// // TASK: DEFINE YOUR FILTER FUNCTION BELOW:
+// function filter(arr,fn) {
+//   const newArray=[];
+//   for(let i=0;i<arr.length;i++){
+//     if(fn(arr[i])){
+//       newArray.push(arr[i]);
+//     }
+//   }
+//   return newArray;
 
-}
-function checkFirstLetter(word){
-  return word[0]==='R';
-}
+// }
+// function checkFirstLetter(word){
+//   return word[0]==='R';
+// }
 
-console.log(filter(myNames,checkFirstLetter));
-console.log(myNames.filter(name=>name[0]==='R'));
+// console.log(filter(myNames,checkFirstLetter));
+// console.log(myNames.filter(name=>name[0]==='R'));
 
 
 function hazardWarningCreator(typeOfWarning){
@@ -57,13 +57,8 @@ function hazardWarningCreator(typeOfWarning){
   return location => {
     warningCounter++;
     console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
-    // console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} ${warningCounter === 1 ? 'time' : 'times' today!`)
-    if (warningCounter===1){
-      console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time today!`);
-    }
-    else {
-      console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} times today!`);
-  };
+    console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} ${warningCounter === 1 ? 'time' : 'times'} today!`);
+  }
 }
 
 const warning1 = hazardWarningCreator("Rocks on the road");
@@ -105,3 +100,12 @@ function secretMessage(str){
 }
 
 console.log(secretMessage(secretWords));
+
+function secretMessageRefactor(str){
+  return str.split(' ')
+            .reduce((total, current) => {
+              return current.length === 3 ? total + ' ' : total + current[current.length - 1].toUpperCase();
+            }, '');
+}
+
+console.log(secretMessageRefactor(secretWords));
